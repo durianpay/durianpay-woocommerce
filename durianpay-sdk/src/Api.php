205a39786102a0ca055f2232463c50f91507c200
Anuj Verma <anuj@joshsoftware.com>
2021-04-09 13:25:42 +0530

    Updated URL to production

--- durianpay-sdk/src/Api.php
+++ durianpay-sdk/src/Api.php
@@ -4,7 +4,7 @@ namespace Durianpay\Api;
 
 class Api
 {
-    protected static $baseUrl = 'http://api-staging.durianpay.id/';
+    protected static $baseUrl = 'https://api.durianpay.id/';
 
     protected static $key = null;
 
